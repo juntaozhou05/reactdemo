12017e2e02d55c7d34b7c9397775f861045338e3
zhoujuntao <350529307@qq.com>
2018-02-11 23:17:09 +0800

    updata redux

---
+++ src/components/Counter.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        
+        return ( 
+            <div className="counter">
+                <input type="text"/>
+            </div>
+         )
+    }
+}
+ 
+export default Counter;
